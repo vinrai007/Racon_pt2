36e52a728f844a9af5d68502a4891a9d2bedc9a8
vinrai007 <129852101+vinrai007@users.noreply.github.com>
2024-08-11 00:59:50 +0530

    first commit

---
+++ src/routes/homepage/Homepage.jsx
@@ -0,0 +1,82 @@
+import { Link } from "react-router-dom";
+import "./homepage.css";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
+
+const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("human1");
+
+  return (
+    <div className="homepage">
+      <img src="/orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1>RACON</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
+          dolorem doloribus, architecto dolor.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "human2"
+                  ? "/human2.jpeg"
+                  : "bot.png"
+              }
+              alt=""
+            />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "YOU: Any advice for the tech team?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "RACON: Intra team RACI to be defined in above call​",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "YOU: Any adive for the Marketing team?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "RACON: Too much noise dissolves the key highlight you want to make​",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
